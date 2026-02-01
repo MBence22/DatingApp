46db4f857134e3e17e16bc141722eefadefed139
BlackFox <144833835+MBence22@users.noreply.github.com>
2026-02-01 10:40:03 +0100

    End of Section 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating App';
+  protected members = signal<any>([])
+  
+  
+  // ngOnInit(): void {
+  //   this.http.get('https://localhost:5001/api/members').subscribe({
+  //     next: response => this.members.set(response),
+  //     error: error => console.log(error),
+  //     complete: () => console.log('Completed the http request')
+  //   }) // on complition unsubscribe automatically;
+  // }
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members')); // await is not needed, because it returns a promise
+    } catch(error){
+      console.log(error);
+      throw error;
+    }
+  }
+}
